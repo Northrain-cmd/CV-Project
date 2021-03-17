7d9ef5d77848d5d10ab6f02f5298708c30c5e502
Northrain-cmd <northrain@icloud.com>
2021-03-17 11:30:04 +0300

    Styling improved

--- src/components/Education.js
+++ src/components/Education.js
@@ -50,31 +50,33 @@ class Education extends React.Component{
   render() {
     // eslint-disable-next-line no-unused-vars
     const {_edit, school, city, from, to, degree, desc } = this.state;
-    const display = <div className="d-flex flex-column align-items-center display mt-4">
-      <h5><span>{school}</span></h5>  
-      <h5 className="mb-4 mt-2"><span>{city}</span></h5>  
-      <h5 className="mb-4"><span>{from}</span></h5>
-      <h5 className="mb-4"><span>{to}</span></h5>
-      <h5 className="mb-4"><span>{degree}</span></h5>
-      <p className="mb-4">{desc}</p>
-      <button className="btn btn-success mt-4 p-2 w-50" type="button" onClick={this.onEdit}>Edit</button>
+    const display = <div className="d-flex flex-column align-items-center display-edu mt-4">
+      <div className="container">
+        <h5 className="mb-4 lead">Institution: <span>{school}</span></h5>  
+        <h5 className="mb-4 lead">City: <span>{city}</span></h5>  
+        <h5 className="mb-4 lead">From: <span>{from}</span></h5>
+        <h5 className="mb-4 lead">To: <span>{to}</span></h5>
+        <h5 className="mb-4 lead">Qualification: <span>{degree}</span></h5>
+         {desc && <p className="mb-4"><span className="lead">Additional info: </span>{desc}</p> }
+      </div>
+      <button className="btn btn-outline-success mb-4 p-2 w-50" type="button" onClick={this.onEdit}>Edit</button>
     </div>;
 
     return (
       this.state.edit ? 
-      <form className="px-lg-5 px-md-3" onSubmit={this.onSubmit} id="education-form">
+      <form className="px-lg-5 px-md-3 mb-4" onSubmit={this.onSubmit} id="education-form">
         <label htmlFor="school">School</label>
         <input placeholder="University name" required className="mb-2" value={this.state.school}  onChange={this.onChange} id="school" name="school"></input>
         <label htmlFor="city">City</label>
         <input placeholder="City" className="mb-2" value={this.state.city}  onChange={this.onChange} id="city" name="city" required></input>
         <label htmlFor="from">From</label>
-        <input placeholder="YYYY" required className="mb-2" value={this.state.from}  onChange={this.onChange} id="from" name="from"></input>
+        <input type="number" placeholder="YYYY" max="2200" min="1900" required className="mb-2" value={this.state.from}  onChange={this.onChange} id="from" name="from"></input>
         <label htmlFor="to">To</label>
-        <input placeholder="YYYY" className="mb-2" value={this.state.to}  onChange={this.onChange} id="to" name="to"></input>
+        <input type="number" placeholder="YYYY" max="2200" min="1900" className="mb-2" value={this.state.to}  onChange={this.onChange} id="to" name="to"></input>
         <label htmlFor="degree">Degree or stream</label>
         <input placeholder="Qualification" className="mb-2" value={this.state.degree}  onChange={this.onChange} id="degree" name="degree"></input>
-        <textarea placeholder="Anything you want to add, but try to keep it simple" onChange={this.onChange} id="desc" name="desc" className="mb-2 mt-2" value={this.state.desc}></textarea>
-        <button className="btn btn-success p-2 mx-auto w-50 mt-3" type="submit">Submit</button>
+        <textarea placeholder="Anything you want to add, but try to keep it short" onChange={this.onChange} id="desc" name="desc" className="mb-2 mt-2" value={this.state.desc}></textarea>
+        <button className="btn btn-outline-success p-2 mx-auto w-50 mt-3" type="submit">Submit</button>
       </form>
       : display
     )
--- src/components/General.js
+++ src/components/General.js
@@ -67,13 +67,18 @@ class General extends React.Component {
     // eslint-disable-next-line no-unused-vars
     const {_edit, name, phone, email, git } = this.state;
     const display = <div className="d-flex flex-column align-items-center display mt-4">
-      <h4 className="mx-auto my-4">General Information</h4>
-      <h5><span>{name}</span></h5>  
-      {this.state.country && <CountrySelect className="mb-2" id="country" value={this.state.country} disabled/> } 
-      <h5 className="mb-4 mt-2"><FontAwesomeIcon color="green" className="mr-2" size="lg" icon={faMobileAlt} /> <a href={"tel:"+phone}>{phone}</a></h5>  
-      <h5 className="mb-4"><FontAwesomeIcon color="green" className="mr-1" size="lg" icon={faEnvelope} />  <a href={"mailto:" + email}>{email}</a></h5>  
-      {this.state.git && <h5 className="mb-2"><FontAwesomeIcon color="green" className="mr-2" size="lg" icon={faGithubAlt} /><a href={git}>Github</a></h5> }  
-      <button className="btn btn-success mt-4 p-2 w-50" type="button" onClick={this.onEdit}>Edit</button>
+      <div className="container">
+        <h4 className="mx-auto my-4 text-center">General Information</h4>
+        <h5><span>{name}</span></h5>    
+        {this.state.country && <CountrySelect className="mb-2" id="country" value={this.state.country} disabled/> } 
+        <h5 className="mb-4 mt-4"><FontAwesomeIcon color="green" className="mr-4 ml-1" size="lg" icon={faMobileAlt} /> <a href={"tel:"+phone}>{phone}</a></h5>  
+        <hr></hr>
+        <h5 className="mb-4"><FontAwesomeIcon color="orange" className="mr-3" size="lg" icon={faEnvelope} />  <a href={"mailto:" + email}>{email}</a></h5>  
+        <hr></hr>
+        {this.state.git && <h5 className="mb-2"><FontAwesomeIcon color="indigo" className="mr-4" size="lg" icon={faGithubAlt} /><a href={git}>Github</a></h5> }  
+      </div>
+      
+      <button className="btn btn-outline-success mt-4 p-2 w-50" type="button" onClick={this.onEdit}>Edit</button>
     </div>;
 
     return (
@@ -81,7 +86,7 @@ class General extends React.Component {
       <form className="px-lg-5 px-md-3" onSubmit={this.onSubmit} id="general-form">
         <label className="mx-auto mt-2 h4" htmlFor="general-form">General Information</label>
         <label htmlFor="name">Name</label>
-        <input placeholder="Your first and last names" required className="mb-2" value={this.state.name}  onChange={this.onChange} id="name" name="name"></input>
+        <input placeholder="Your first and last name" required className="mb-2" value={this.state.name}  onChange={this.onChange} id="name" name="name"></input>
         <label htmlFor="country">Country</label>
         <CountrySelect noMatchesText="" className="mb-2" name="country" id="country" value={this.state.country} onChange={this.onChange}/>
         <label htmlFor="phone">Phone</label>
@@ -90,7 +95,7 @@ class General extends React.Component {
         <input placeholder="myemail@example.com" type="email" required className="mb-2" value={this.state.email}  onChange={this.onChange} id="email" name="email"></input>
         <label htmlFor="git">Github Link</label>
         <input placeholder="Link your Github account (optional)" className="mb-2" value={this.state.git}  onChange={this.onChange} id="git" name="git"></input>
-        <button className="btn btn-success p-2 mx-auto w-50 mt-3" type="submit">Submit</button>
+        <button className="btn btn-outline-success p-2 mx-auto w-50 mt-3" type="submit">Submit</button>
       </form>
       : display
     )
